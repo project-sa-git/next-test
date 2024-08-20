7ce9b5fc91d31c6371abddd1104e50e59a08a851
mymac <trybook1901@gmail.com>
2024-08-21 06:51:10 +0900

    404ページ追加

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// オリジナルの404ページ作成
+// http://localhost:3000/naiyokoreee
+// お探しのページがありません
+
+const NotFound = () => {
+  return <div>お探しのページがありません</div>;
+};
+
+export default NotFound;
