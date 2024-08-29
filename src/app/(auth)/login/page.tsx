aa29b349dd53400489f44d3b0e82dc6f131c2988
mymac <trybook1901@gmail.com>
2024-08-30 02:04:04 +0900

    グルーピング

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const page = () => {
+  // グルーピング：ルートグループ((xxx))
+  // auth配下の子供が増えたり減ったりしない場合等(loginなど)に利用
+  // http://localhost:3000/login
+  return <div>ログイン</div>;
+};
+
+export default page;
---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>サインイン</div>;
+};
+
+export default page;
