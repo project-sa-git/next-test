147a3c5816afcc37d419a844da79568155591857
mymac <trybook1901@gmail.com>
2024-09-01 01:53:35 +0900

    titleタグとメタディスクリプションを設定

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+// layout.tsxとpage.tsxを両方書いたときにはpage.tsxが優先される
+export const metadata = {
+  title: "私たちについて",
+  description: "当社についての概要です",
+};
+
 export const page = () => {
   return <div>アバウトpage</div>;
 };
