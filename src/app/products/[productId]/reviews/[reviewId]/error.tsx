dbd471cc8d73b5baa91b21202cd9a64dcf2d52d8
mymac <trybook1901@gmail.com>
2024-09-02 14:50:14 +0900

    エラー画面を作る(error.tsx)

---
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const Error = () => {
+  return <div>読み込みエラーです</div>;
+};
+
+export default Error;
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 
+const getRandomInt = (count: number) => {
+  return Math.floor(Math.random() * count);
+};
 export const page = ({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) => {
+  // 0か1を返すだけの関数を実行
+  const random = getRandomInt(2);
+  console.log(random);
+  if (random === 1) {
+    throw new Error("エラー");
+  }
+
   return (
     <div>
       商品名:{params.productId}のレビュー{params.reviewId}
